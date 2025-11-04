c3f078d67d1088a92970c83b62f3f281f341dfd3
ZVinhZ <nxvinh2101@gmail.com>
2025-11-04 19:11:39 +0700

    Update utils.py for logging setup

--- src/utils.py
+++ src/utils.py
@@ -12,6 +12,29 @@ LOG_DIRECTORY.mkdir(exist_ok = True)
 # Hide DEBUG which is only for developer
 def setup_logger(name: str = "pair trading", level = logging.INFO):
     logger = logging.getLogger(name)
+    
+    # check whether the logger already has any handlers attached
+    # avoid duplicate logger
+    if logger.handlers:
+        return logger
+    
+    logger.setLevel = level
 
+    format_logger = logging.Formatter("%(asctime)s %(levelname)s [%(name)s] %(message)s")
+    
+    # console handler used for sending log message to the terminal
+    # log appears live in the terminal
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(format_logger)
+
+    logger.addHandler(console_handler)
+
+    # file handler used for saving log into a file
+    file_handler = logging.FileHandler(LOG_DIRECTORY / f"{name}.log")
+    file_handler.setFormatter(format_logger)
+    
+    logger.addHandler(file_handler)
+
+    return logger
 
 
